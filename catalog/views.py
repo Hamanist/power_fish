3195ec110f180d9bc0a1c7fb40ea079d4a9226df
Hamanist <hamanist@gmail.com>
2026-03-13 11:08:48 +0300

    mini fix

--- catalog/views.py
+++ catalog/views.py
@@ -5,4 +5,10 @@ def index(request):
 
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        massage = request.POST.get('massage')
+        print(f'{name} email - {email}, сообщение - ({massage})')
+
     return render(request, 'catalog/contacts.html')
